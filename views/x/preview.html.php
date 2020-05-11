7d343667dc4e0d58c5d91e87c2b96ada418ef47c
Nilam Doctor <nilamdoc@gmail.com>
2020-05-11 13:06:42 +0530

    Azadi

--- views/x/preview.html.php
+++ views/x/preview.html.php
@@ -1,30 +1,18 @@
 <div class="text-align-center">
-	<div><h3>Business Plan</h3>
-	<a href="https://app.box.com/s/o0rm9kr1yusfoiitea9klyzfgwb4encz" class="link external">Download</a>
+<p class="Bebas sz1">Thank you for attending our discovery session. We request you to go through the product catalogue and company profile 
+</p>
+			<div><h3>Sapne - Azadipdf</h3>
+	<a href="https://sff.team/img/documents/Azadi-Sapne.pdf" class="link external">Download</a>
 	</div>
-	<div><h3>Top 20 SPBM.pdf</h3>
-	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
-	</div>
-	<div><h3>BOM Flip Chart Hindi SPBM.pdf</h3>
-	<a href="https://app.box.com/s/espzxnmrlweshjxk2c4v1bxc0187ylzr" class="link external">Download</a>
-	</div>
-			<div><h3>Velocity SPBM.pdf</h3>
-	<a href="https://app.box.com/s/zm242wa1lk5syz28q97st4zvn4vx9mbs" class="link external">Download</a>
-	</div>
-			<div><h3>Fruit of the Earth SPBM.pdf</h3>
-	<a href="https://app.box.com/s/5kden2a5kucpk1s7c1a6nsb9zg660uzp" class="link external">Download</a>
-	</div>
-			<div><h3>Fresh Moments SPBM.pdf</h3>
-	<a href="https://app.box.com/s/zvtluyvt41qnzbihe2tfuak5s1uwxq16" class="link external">Download</a>
-	</div>
-			<div><h3>Auto Care SPBM.pdf</h3>
-	<a href="https://app.box.com/s/bujds7fgjzs3xsjzf9pvuhiht8a4t2zy" class="link external">Download</a>
-	</div>
+
 			<div><h3>Personal Care SPBM.pdf</h3>
 	<a href="https://app.box.com/s/jwxlfluxxf18kkqbd3t474298f8ielpp" class="link external">Download</a>
 	</div>
 			<div><h3>Home Care SPBM.pdf</h3>
 	<a href="https://app.box.com/s/y9dkuuizejupwi4113g5ixyt42sdhf12" class="link external">Download</a>
+	</div>
+			<div><h3>Fruit of the Earth SPBM.pdf</h3>
+	<a href="https://app.box.com/s/5kden2a5kucpk1s7c1a6nsb9zg660uzp" class="link external">Download</a>
 	</div>
 			<div><h3>Urban Color SPBM.pdf </h3>
 	<a href="https://app.box.com/s/vf2vcjwf4zci1imcnabqyl7it86btrrg" class="link external">Download</a>
@@ -35,20 +23,27 @@
 			<div><h3>WELL Product Training SPBM.pdf</h3>
 	<a href="https://app.box.com/s/lb1ta2onf5khsujxwj7pplcdx9q6t48e" class="link external">Download</a>
 	</div>
+			<div><h3>Fresh Moments SPBM.pdf</h3>
+	<a href="https://app.box.com/s/zvtluyvt41qnzbihe2tfuak5s1uwxq16" class="link external">Download</a>
+	</div>
+
+	<div><h3>BOM Flip Chart Hindi SPBM.pdf</h3>
+	<a href="https://app.box.com/s/espzxnmrlweshjxk2c4v1bxc0187ylzr" class="link external">Download</a>
+	</div>
+	<div><h3>Business Plan</h3>
+	<a href="https://app.box.com/s/o0rm9kr1yusfoiitea9klyzfgwb4encz" class="link external">Download</a>
+	</div>
+	<div><h3>Top 20 SPBM.pdf</h3>
+	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
+	</div>
+			<div><h3>Velocity SPBM.pdf</h3>
+	<a href="https://app.box.com/s/zm242wa1lk5syz28q97st4zvn4vx9mbs" class="link external">Download</a>
+	</div>
+			<div><h3>Auto Care SPBM.pdf</h3>
+	<a href="https://app.box.com/s/bujds7fgjzs3xsjzf9pvuhiht8a4t2zy" class="link external">Download</a>
+	</div>
 			<div><h3>NCO NEW - FINAL SPBM.pdf</h3>
 	<a href="https://app.box.com/s/5hq8u6mjmlyfo5nyh09n41mlwcaaemp2" class="link external">Download</a>
-	</div>
-			<div><h3>Top 20 SPBM.pdf</h3>
-	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
-	</div>
-			<div><h3>Top 20 SPBM.pdf</h3>
-	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
-	</div>
-			<div><h3>Top 20 SPBM.pdf</h3>
-	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
-	</div>
-			<div><h3>Top 20 SPBM.pdf</h3>
-	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
 	</div>
 				<div><h3>Loyalty.pdf</h3>
 	<a href="https://app.box.com/s/rfl4b548hb4dq9zbnmi0b53y93biue22" class="link external">Download</a>
