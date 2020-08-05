69e1971f0c82678f738d085056de21349f706695
Nilam Doctor <nilamdoc@gmail.com>
2020-08-05 09:27:32 +0530

    im

--- views/y/upload.html.php
+++ views/y/upload.html.php
@@ -2,6 +2,6 @@
  <div class="uplink"><a href="/y/index" class="link external"><h3>New Upload</h3></a></div>
  <div class="content">
   <!-- Display uploaded video info -->
-  <?php echo $htmlBody; ?>
+  <?=$htmlBody;?>
  </div>
 </div>
