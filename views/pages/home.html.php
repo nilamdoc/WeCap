1c9c5955c0b497f745f1ae824a42e3fce20f19d1
Nilam Doctor <nilamdoc@gmail.com>
2021-01-07 06:09:19 +0530

    site

--- views/pages/home.html.php
+++ views/pages/home.html.php
@@ -11,13 +11,84 @@
  
  <div class="Banner2-Section" id="info">
  <p>&nbsp;</p>
- <div class="Banner2-Title">Sparkle your Professional Career</div>
- <div class="Banner2-Para">You are an SME or are have a professional career or a rising star, all of us face challenges, one way or other. Everyone has an ebb and a tide, occassionaly in their life. WeCapacitate you, we entitle, we qualify, we enable you to a new level of opportunites. No barriers in your life are impossible to overcome.
+ <div class="Banner2-Title">Sparkle your Business & Professional Career</div>
+ <div class="Banner2-Para">You are an SME or are have a professional career or a rising star, all of us face challenges, one way or other. Everyone has an ebb and a tide, occassionaly in their life. <strong><span class="maroon">We</span><span class="gray">C</span><span class="maroon">an</span></strong>, <strong><span class="maroon">We</span><span class="gray">C</span><span class="maroon">ommit</span></strong>, <strong><span class="maroon">We</span><span class="gray">C</span><span class="maroon">hange</span></strong>, <strong><span class="maroon">We</span><span class="gray">C</span><span class="maroon">apacitate</span></strong> you, we entitle, we qualify, we enable you to a new level of opportunites. No barriers in your life are impossible to overcome.
  <span>Motivation is available all across the internet. We transform you first. You analyze yourself. Your power is your mind. <strong>10<sup>x</sup></strong> it. Improve your decisions. You will always in excel in every situation.
  </span>
-
  The most powerful, proven system will teach you right from understanding who you are as a person all the way to creating your ultimate Success Blueprint. And that’s not all, after all this, we’re working with you to ensure you’re producing results!</div>
  <div class="Banner2-SubTitle"><a href="#process" class="link external " ><span class="Banner2-button">Let’s see how it works</span></a></div>
  </div>
+
+ <div class="process" id="process">
+  <div class="timeline block">
+   <div class="timeline-item ">
+     <div class="timeline-item-divider"></div>
+     <div class="timeline-item-content card elevation-6 elevation-hover-24 elevation-pressed-12 elevation-transition">
+       <div class="timeline-item-inner">
+         <div class="card-header process-title">Ready</div>
+         <div class="card-content card-content-padding process-subtitle">Get Set, Go<br><img src="/img/stick01.png"><br>You are alone</div>
+         <div class="card-content theprocess-para">You simply have to register with us. We don’t take anyone and everyone. We want people who are driven to help others and even before that, ready to go through the transformation themselves!</div>
+       </div>
+     </div>
+   </div>
+   <div class="timeline-item">
+     
+     <div class="timeline-item-divider"></div>
+     <div class="timeline-item-content card elevation-6 elevation-hover-24 elevation-pressed-12 elevation-transition">
+       <div class="timeline-item-inner">
+         <div class="card-header process-title">Explore, Learn</div>
+         <div class="card-content card-content-padding process-subtitle">10 week course<br><img src="/img/stick02.png"><br>You Join the Team</div>
+         <div class="card-content theprocess-para"><p>Building blocks is a science.</p>
+            <p>In the first 5 weeks, we will train you from A to Z. Literally everything! We will put you on a Fast Start Program for a boosted head start and we will give you a full step by step training on how to first transform yourself and then how to use the same system to transform others!</p>
+            <p>Next 5 weeks training, we will not only give you access all the tools but we will also train you on how to use them effectively!</p>
+            <p>The training is a combination of online coaching system + weekly meetings + assignments + masterminds to keep you on track. You cannot go wrong!</p></div>
+       </div>
+     </div>
+   </div>
+   <div class="timeline-item">
+     
+     <div class="timeline-item-divider"></div>
+     <div class="timeline-item-content card elevation-6 elevation-hover-24 elevation-pressed-12 elevation-transition">
+       <div class="timeline-item-inner">
+         <div class="card-header process-title">Time to Express</div>
+         <div class="card-content card-content-padding process-subtitle">You got it Right<br><img src="/img/stick03.png"><br>You Grow</div>
+         <div class="card-content theprocess-para"><p>Your foundation is stronger than ever</p>
+            <p>With a strong foundation you can now surpass all your limiting factors. Your success blue print is with you always.</p>
+            <p>We guide you to make presentations, seminars, and expand your market reach with innovation.</p>
+            <p></p></div>
+       </div>
+     </div>
+   </div>
+   <div class="timeline-item">
+     
+     <div class="timeline-item-divider"></div>
+     <div class="timeline-item-content card elevation-6 elevation-hover-24 elevation-pressed-12 elevation-transition">
+       <div class="timeline-item-inner">
+         <div class="card-header process-title">Experience</div>
+         <div class="card-content card-content-padding process-subtitle">Success is contagious<br><img src="/img/stick04.png"><br>You Lead a Team<br><span class="Banner-Sub">The UnStoppable You</span></div>
+         <div class="card-content theprocess-para"><p>Change is the only contant</p>
+            <p>Gestalt -     an organized whole that is perceived as more than the sum of its parts.</p>
+            <p>You as a team are now Un-breakable. No chance of failure.</p>
+            <p></p></div>
+       </div>
+     </div>
+   </div>
+  </div>
+ </div>
+
+   <div class="Banner3-Section" id="info2">
+   <p>&nbsp;</p>
+   <div class="Banner3-Title">Success! Your Blue Print Shines</div>
+   <div class="Banner3-Para">Create you own MasterMind group, nationally. Be recognized! 
+   <span>Your story of success can be replicated with the system we have. You are your own leader. All the obstacles in you future profession are destroyed before you notice them. You are  <strong>10<sup>x</sup></strong> stronger. </span>
+
+   <br>You are also ready to duplicate the system.</div>
+   <div class="Mastermind-video">
+      <iframe src="https://player.vimeo.com/video/341523301?title=0&byline=0&portrait=0" width="800" height="600" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+   </div>
+ </div>
+ 
+ 
+
 
 </div>
